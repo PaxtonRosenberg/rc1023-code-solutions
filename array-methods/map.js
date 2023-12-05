aaca2879aa2e1872fb958916dce6b2631fce0cc0
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2023-12-05 08:15:58 +0000

    Added toFixed method on the discountedPrices function in maps.js

--- array-methods/map.js
+++ array-methods/map.js
@@ -1,7 +1,7 @@
 const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
 
 const discountedPrices = prices.map(
-  (price) => `{price: ${price}, salePrice: ${price / 2}}`
+  (price) => `{price: ${price.toFixed(2)}, salePrice: ${price.toFixed(2) / 2}}`
 );
 console.log('Price objects:', discountedPrices);
 
