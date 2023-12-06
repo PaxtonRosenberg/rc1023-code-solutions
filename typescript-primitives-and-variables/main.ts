7f7ef31db17fa540497aec1b505aa959d9941699
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2023-12-06 19:09:12 +0000

    Completed typescript-primitives-and-variables exercise

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,16 @@
+const fullName: string = 'Paxton Rosenberg';
+const isCool: boolean = true;
+const totalPets: number = 3;
+
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+const unknown: null = null;
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+let never: undefined;
+console.log('never:', never);
+console.log('typeof never:', typeof never);
