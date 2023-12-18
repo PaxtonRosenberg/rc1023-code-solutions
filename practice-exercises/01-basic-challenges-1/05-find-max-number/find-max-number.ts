509975dd4ea43b3610f49412f50b5246da0a3bb5
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2023-12-18 19:07:46 +0000

    Completed practice-exercises 01-basic-challenges-1

---
+++ practice-exercises/01-basic-challenges-1/05-find-max-number/find-max-number.ts
@@ -0,0 +1,10 @@
+export function findMaxNumber(arr: number[]): number {
+  let maxNum: number = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxNum) {
+      maxNum = arr[i];
+    }
+  }
+  return maxNum;
+}
