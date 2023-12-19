c34032e1ac124d12834a8580b0e1aae45c8de1aa
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2023-12-19 00:38:50 +0000

    Practice exercises 02-basic-challenges 1-4

--- practice-exercises/02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.ts
+++ practice-exercises/02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.ts
@@ -1,3 +1,16 @@
 export function fizzBuzzArray(num: number): (string | number)[] {
-  return [];
+  const newArray: (string | number)[] = [];
+
+  for (let i = 1; i < num + 1; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      newArray.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      newArray.push('Buzz');
+    } else if (i % 3 === 0) {
+      newArray.push('Fizz');
+    } else {
+      newArray.push(i);
+    }
+  }
+  return newArray;
 }
--- practice-exercises/02-basic-challenges-2/04-find-missing-number/find-missing-number.ts
+++ practice-exercises/02-basic-challenges-2/04-find-missing-number/find-missing-number.ts
@@ -1,3 +1,10 @@
 export function findMissingNumber(arr: number[]): number | undefined {
-  return undefined;
+  let sum: number = 0;
+  let missingNum: number = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  missingNum = sum - arr.length;
+  return missingNum;
 }
