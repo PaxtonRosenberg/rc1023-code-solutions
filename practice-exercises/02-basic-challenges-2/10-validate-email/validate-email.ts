dc36ca016e9311060e9c9d9ada94207b2a08aeff
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2024-01-02 17:54:09 +0000

    Completed 02-basic-challenges-2 folder

--- practice-exercises/02-basic-challenges-2/10-validate-email/validate-email.ts
+++ practice-exercises/02-basic-challenges-2/10-validate-email/validate-email.ts
@@ -1,3 +1,5 @@
 export function validateEmail(email: string): boolean {
-  return false;
+  const validEmail: RegExp = /^[a-zA-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
+
+  return validEmail.test(email);
 }
