79a02a35db59e896d84daa3f15bc74d1f7cfa170
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2024-01-04 18:14:54 +0000

    Completed 03-high-order-array-methods numbers 1-2

--- practice-exercises/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.ts
+++ practice-exercises/03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.ts
@@ -1,3 +1,11 @@
 export function sumOfEvenSquares(numbers: number[]): number {
-  return 0;
+  const evenNums = numbers.filter((number) => number % 2 === 0);
+  const squaredEvens = evenNums.map((evenNum) => Math.pow(evenNum, 2));
+  const sumOfSquaredEvens = squaredEvens.reduce((total, squaredEven) => {
+    return total + squaredEven;
+  }, 0);
+  return sumOfSquaredEvens;
 }
+
+const numbers = [1, 2, 3, 4, 5];
+sumOfEvenSquares(numbers);
