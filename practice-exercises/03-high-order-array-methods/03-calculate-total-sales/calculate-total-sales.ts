f7fc13e8f5f50a7be92098becf46fff74941856b
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2024-01-05 22:05:38 +0000

    Completed 03-high-order-array-methods numbers 3 and 4

--- practice-exercises/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.ts
+++ practice-exercises/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.ts
@@ -8,5 +8,13 @@ export function calculateTotalSalesWithTax(
   products: Product[],
   taxRate: number
 ): number {
-  return 0;
+  const price = products
+    .map((product) => product.price * product.quantity)
+    .reduce((totalPrice, price) => totalPrice + price, 0);
+
+  const tax = (taxRate / 100) * price;
+
+  const priceWithTax = price + tax;
+
+  return priceWithTax;
 }
