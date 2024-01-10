a05cc626abb4d66b62126e1af00744fc0c46e626
Paxton Rosenberg <rosenbergpaxton@gmail.om>
2024-01-10 19:32:16 +0000

    Updated error handling, removed ternary operator due to issue with throw keyword and replaced with if statement

--- fetch/main.js
+++ fetch/main.js
@@ -1,11 +1,13 @@
 async function getUsers() {
   try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    !res.ok ? new Error() : console.log('response:', res.ok);
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/');
+    if (!res.ok) {
+      throw new Error(`Error, failed to fetch: ${res.status}`);
+    }
     const users = await res.json();
     console.log('users:', users);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
   }
 }
 
@@ -14,11 +16,13 @@ getUsers();
 async function getPokemon() {
   try {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-    !res.ok ? new Error() : console.log('response:', res.ok);
+    if (!res.ok) {
+      throw new Error(`Error, failed to fetch: ${res.status}`);
+    }
     const pokemon = await res.json();
     console.log('pokemon:', pokemon);
   } catch (err) {
-    console.err(err);
+    console.error(err.message);
   }
 }
 
